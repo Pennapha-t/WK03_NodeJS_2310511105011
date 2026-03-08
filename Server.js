680f5d4b0b716e258f38de9c3cfbf91c68000fd7
Pennapha-t <pastelsky.k@gmail.com>
2026-03-08 17:01:10 +0700

    update code

--- Server.js
+++ Server.js
@@ -2,11 +2,11 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-    // แยก Path และ Query String ออกจากกัน
+  
     const parsedUrl = url.parse(req.url, true);
     res.setHeader('Content-Type', 'application/json');
 
-    // กรณี GET / หรือ GET /?message=hi
+   
     if (req.method === 'GET' && parsedUrl.pathname === '/') {
         if (parsedUrl.query.message === 'hi') {
             res.end(JSON.stringify({ msg: "Hello, How are you?" }));
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
         }
     }
 
-    // กรณี POST /api/sayhi
+   
     else if (req.method === 'POST' && parsedUrl.pathname === '/api/sayhi') {
         let body = '';
         req.on('data', chunk => { body += chunk; });
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
-// เปิด Port 9818 ตามโจทย์ของอาจารย์
+
 server.listen(9818, () => {
     console.log('Server is running on http://localhost:9818');
 });
